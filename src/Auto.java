02094615d56544cba38c7865257fa7a119aab12b
Nikita <athlon64x2600@gmail.com>
2023-01-30 11:00:25 +0200

    Задание 2     В поле объекта использован конструктор.     Свойства объектов передаются с помощью конструктора.     В консоль выведено сообщение о каждом автомобиле с перечислением всех их характеристик.

--- src/Auto.java
+++ src/Auto.java
@@ -6,6 +6,15 @@ public class Auto {
     int year;
     String country;
 
+    public Auto(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return
--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Auto ladaGranta = new Auto();
-
-        ladaGranta.brand = "Lada";
-        ladaGranta.model = "Granta";
-        ladaGranta.year = 2015;
-        ladaGranta.country = "Russia";
-        ladaGranta.color = "Yellow";
-        ladaGranta.engineVolume = 1.7;
-
-        Auto audiA8 = new Auto();
-
-        audiA8.brand = "Audi";
-        audiA8.model = "A8 50 L TDI quattro";
-        audiA8.year = 2020;
-        audiA8.country = "Germany";
-        audiA8.color = "Black";
-        audiA8.engineVolume = 3.0;
-
-        Auto bmwZ8 = new Auto();
-
-        bmwZ8.brand = "BMW";
-        bmwZ8.model = "Z8";
-        bmwZ8.year = 2021;
-        bmwZ8.country = "Germany";
-        bmwZ8.color = "Black";
-        bmwZ8.engineVolume = 3.0;
-
-        Auto kia = new Auto();
-
-        kia.brand = "KIA";
-        kia.model = "Sportage 4-го поколения";
-        kia.year = 2018;
-        kia.country = "South Korea";
-        kia.color = "Red";
-        kia.engineVolume = 3.0;
-
-        Auto hyundai = new Auto();
-
-        hyundai.brand = "hyundai";
-        hyundai.model = "Avante";
-        hyundai.year = 2016;
-        hyundai.country = "South Korea";
-        hyundai.color = "Orange";
-        hyundai.engineVolume = 1.6;
+        Auto ladaGranta = new Auto("Lada", "Granta", 1.7, "Yellow",2015 ,"Russia");
+        Auto audiA8 = new Auto("Audi", "A8 50 L TDI quattro", 3.0, "Black" , 2020, "Germany");
+        Auto bmwZ8 = new Auto("BMW", "Z8", 3.0, "Black", 2021, "Germany");
+        Auto kia = new Auto("KIA", "Sportage 4-го поколения", 3.0, "Red", 2018, "South Korea");
+        Auto hyundai = new Auto("Hyundai", "Avante", 1.6, "Orange", 2016, "South Korea");
 
         System.out.println(ladaGranta);
         System.out.println(audiA8);
